e0e2524b0aae625e33666174046ef8263929a0ba
root <linusmar13@gmail.com>
2022-12-19 10:53:44 -0800

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,12 @@
  * swap_int - swaps the values of two integers
  * @a: integer to swap
  * @b: integer to swap
-void swap_int(int *a, int *b)
 */
+
 void swap_int(int *a, int *b)
 
 {
-		int;
+		int m;
 
 		m = *a;
 		*a = *b;
